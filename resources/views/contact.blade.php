dc2f0fdad9fd7bc0736ebfce34f5f759ef65889c
DAYA ALEM <90417872+kiminoanime@users.noreply.github.com>
2022-10-26 17:02:13 +0200

    page contact daya

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,13 +6,64 @@
         {{--        <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Tabby_cat_with_blue_eyes-3336579.jpg" alt="">--}}
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12 ">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Page contact
+                <div class="p-6 bg-blue border-b border-gray-200">
+                    CONTACT US
                 </div>
             </div>
+
         </div>
     </div>
+
+
+<div class="grid grid-cols-2 gap-2">
+
+          <!--Formulaire -->
+        <section class="bg-white dark:bg-gray-900 max-w-3xl">
+            <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contacter Nous</h2>
+                <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Vous avez un problème technique ? Vous voulez envoyer des commentaires sur une fonctionnalité ? Besoin de détails sur un concert ? Faites-nous savoir !</p>
+                <form action="#" class="space-y-8">
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre Email</label>
+                        <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="nom@gmail.com" required>
+                    </div>
+                    <div>
+                        <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sujet</label>
+                        <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Dites-nous comment nous pouvons vous aider" required>
+                    </div>
+                    <div class="sm:col-span-2">
+                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Votre Message</label>
+                        <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Ecrivez votre message ici..."></textarea>
+                    </div>
+                    <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Envoyer message</button>
+                </form>
+            </div>
+        </section>
+
+        <div class="flex flex-col space-y-6 .. flex h-screen justify-center items-center text-6xl space-y-12">
+            <div class="flex text-center" >Retrouver Nous sur nos Réseaux Sociaux !</div>
+            <div class="flex content-center"> <img src="{{ asset('icons8-snapchat-squared-48.png') }}"> Snapchat</div>
+            <div class="flex content-center"> <img src="{{ asset('icons8-facebook-50.png') }}"> Facebook </div>
+            <div class="flex content-center"> <img src="{{ asset('icons8-Instagram-64.png') }}"> Instagram</div>
+            <div class="flex content-center"> <img src="{{ asset('icons8-snapchat-squared-48.png') }}"> Adresse</div>
+
+        </div>
+<!--Formulaire -->
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 </x-app-layout>
