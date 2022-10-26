c5e17006af3cc641c059571c2951f58ca5b39709
Ibrahim Anil <ibrahimanilcontactpro@gmail.com>
2022-10-26 14:41:51 +0200

    //
    //

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -6,6 +6,10 @@
         {{--        <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Tabby_cat_with_blue_eyes-3336579.jpg" alt="">--}}
     </x-slot>
 
+    <section class="h-screen bg-gray">
+        
+    </section>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
