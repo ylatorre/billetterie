57ce69eacd8e21425cfb3fdea942f838356ee3b0
Ibrahim Anil <ibrahimanilcontactpro@gmail.com>
2022-10-27 12:06:44 +0200

    Carousel + card

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 fixed bottom-0 w-full z-10">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -13,13 +13,13 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('accueil')" :active="request()->routeIs('accueil')">
-                        {{ __('accueil') }}
+                        {{ __('Accueil') }}
                     </x-nav-link>
                     <x-nav-link :href="route('artiste')" :active="request()->routeIs('artiste')">
-                        {{ __('artiste') }}
+                        {{ __('Artiste') }}
                     </x-nav-link>
                     <x-nav-link :href="route('contact')" :active="request()->routeIs('contact')">
-                        {{ __('contact') }}
+                        {{ __('Contact') }}
                     </x-nav-link>
                 </div>
             </div>
