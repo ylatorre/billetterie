40ad95c6d5cd692446e9b330c6b4ae7a7388c755
Yvan LATORRE <32170221+ylatorre@users.noreply.github.com>
2022-12-12 15:52:17 +0100

    Ajoute de commentaire

--- resources/js/genererpdf.js
+++ resources/js/genererpdf.js
@@ -1,36 +1,41 @@
-console.log("genererpdf")
-try {
+// Affiche "genererpdf" dans la console
+console.log("genererpdf");
 
-// Fonction qui génère un PDF à partir d'un élément HTML
+try {
+    // Déclare la fonction "genererpdf" qui génère un PDF à partir d'un élément HTML
     function genererpdf(prixPlace, Locate, datePlace, idconcert) {
         // Créer un nouvel objet jsPDF
         var pdf = new jsPDF();
 
+        // Récupère l'élément HTML avec l'ID "barcode" + l'ID du concert
         let svg = document.getElementById("barcode" + idconcert)
-        console.log(svg)
-        // pdf.addSVG(svg, 10, 10, {
-        //     width: 200,
-        //     height: 100
-        // });
-        // pdf.canvas.svg(svg, 10, 10);
 
+        // Affiche l'élément HTML dans la console
+        console.log(svg)
+
+        // Ajoute du texte au PDF à la position (80, 10)
         pdf.text(80, 10, "Votre billets est le :")
+
+        // Ajoute du texte au PDF à la position (80, 20) avec la valeur du prix du billet
         pdf.text(80, 20, "Prix : " + prixPlace.innerText);
+
+        // Ajoute du texte au PDF à la position (80, 30) avec la date du concert
         pdf.text(80, 30, "Date : " + datePlace.innerText);
+
+        // Ajoute du texte au PDF à la position (80, 40) avec le lieu du concert
         pdf.text(80, 40, "Lieu : " + Locate.innerText);
-        // pdf.text(text, 10, 40);
-        // Enregistrer le document PDF
+
+        // Enregistre le document PDF
         pdf.save('billete.pdf');
     }
 
-
-// Récupérer tous les boutons "buy"
+    // Récupère tous les éléments HTML avec la classe "buy"
     let tousLesBilletes = document.getElementsByClassName('buy');
 
-// Ajouter un gestionnaire d'événement 'click' pour chaque bouton
+    // Ajoute un gestionnaire d'événement 'click' pour chaque élément HTML avec la classe "buy"
     for (let i = 0; i < tousLesBilletes.length; i++) {
         tousLesBilletes[i].addEventListener('click', function (event) {
-            // Récupérer le parent du bouton et générer le PDF
+            // Récupère le parent de l'élément HTML cliqué
             let billetsClickerPrix   = tousLesBilletes[i].parentNode.parentNode.getElementsByClassName("cost")
             let billetsClickerLocate = tousLesBilletes[i].parentNode.parentNode.getElementsByClassName("location")
             let billetsClickerDate   = tousLesBilletes[i].parentNode.parentNode.getElementsByClassName("date")
@@ -64,11 +69,13 @@ try {
         const number                  = string.match(regex)[0]; // Récupère le premier nombre entier trouvé
         // console.log("Resulat ",number+" " + idconcert + number + billetsClickerNom[0].innerText);
         JsBarcode("#barcode" + idconcert, idconcert + number + billetsClickerNom[0].innerText, {
-            width: 3,
-            height: 60,
+            width: 3, height: 60,
         });
 
     }
 } catch (e) {
 
 }
+
+
+
--- resources/js/recap.js
+++ resources/js/recap.js
@@ -1,22 +1,22 @@
 try {
-    let infoConcert       = getCurrentURL().split('/').pop();
-    let IdConcert         = infoConcert.split(';')[0];
-    let PlaceSelectionner = infoConcert.split(';')[1];
-    let PrixPlace         = infoConcert.split(';')[2];
-    let payBtn            = document.getElementById('payBtn');
-    let arrayPlace        = PlaceSelectionner.split(',')
+    // Récupérer les informations sur le concert à partir de l'URL de la page
+    let infoConcert       = getCurrentURL().split('/').pop(),
+        IdConcert         = infoConcert.split(';')[0],
+        PlaceSelectionner = infoConcert.split(';')[1],
+        PrixPlace         = infoConcert.split(';')[2];
 
+    // Récupérer l'élément DOM du bouton de paiement
+    let payBtn = document.getElementById('payBtn');
 
+    // Décomposer la liste des places sélectionnées en un tableau
+    let arrayPlace = PlaceSelectionner.split(',');
+
+    // Définir une fonction pour récupérer l'URL actuelle
     function getCurrentURL() {
         return window.location.href
     }
 
-// console.log(PrixPlace);
-// axios.get('/recap/' + infoConcert).then(response => {
-//     console.log(response.data);
-// }
-// document.getElementById('barcode'+arrayPlace[i])
-//     console.log(arrayPlace);
+    // Générer les codes à barres pour chaque place sélectionnée
     for (let i = 0; i < arrayPlace.length; i++) {
         console.log(arrayPlace[i], "  ", IdConcert, "  ", arrayPlace[i], "  ", PrixPlace)
         JsBarcode("#barcode" + arrayPlace[i], IdConcert + arrayPlace[i] + PrixPlace, {
@@ -25,6 +25,7 @@ try {
         });
     }
 
+    // Enregistrer un gestionnaire d'événements pour le bouton de paiement
     payBtn.addEventListener('click', function () {
         let cards             = document.getElementById('cards');
         let payment           = document.getElementById('payment');
@@ -33,5 +34,6 @@ try {
         payment.style.display = 'none';
     })
 } catch (e) {
+    // Afficher toutes les erreurs dans la console
     console.log(e);
 }
